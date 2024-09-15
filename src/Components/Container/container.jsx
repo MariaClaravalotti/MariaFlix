9a17e80dea38d4a81cee2200bb273dd8d5831d76
maria clara valotti <valottimaria@gmail.com>
2024-09-15 18:44:48 -0300

    atualização

--- src/Components/Container/container.jsx
+++ src/Components/Container/container.jsx
@@ -1,12 +1,6 @@
 
 import styles from "./Container.module.css"
-const btn = document.querySelector('.btn');
 
-    btn.addEventListener('click', ()=>{
-        btn.style.backgroundColor= '#ffac09'
-        btn.style.boxShadow= '0 0 40px #ffac09'
-        btn.style.transition= '.5s ease'
-    });
 
 
 export default function Container({ clildren }) {
